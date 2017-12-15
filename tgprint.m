1866bda33b7813e4b931fe1005e14856d17c7831
jacobian1128 <31084758+jacobian1128@users.noreply.github.com>
2017-12-15 17:18:17 +0900

    Update tgprint.m

--- tgprint.m
+++ tgprint.m
@@ -1,6 +1,29 @@
 function tgprint(varargin)
+% TGPRINT send an image to a Telegram bot
+%
+% Use tgprintf() in the same way as sprintf()
+% Example: figure(1); plot(x,y);
+%          tgprint();
+% 
+% There are two sending modes:
+% (1) tgprint('photo'): send an image w/ compression using sendPhoto
+% (2) tgprint('document'): send an image w/o compression using sendDocument
+% 
+% Define token and chat_id before use, 
+% which are the authorization token of the target Telegram bot 
+% and the identifier or username of the target chat
+%
+% Please refer the following post 
+% "Creating a Telegram bot for personal notifications"
+% https://www.forsomedefinition.com/automation/creating-telegram-bot-notifications/
+% 
+% This also uses urlreadpost by Dan Eills
+% https://www.mathworks.com/matlabcentral/fileexchange/27189-urlreadpost-url-post-method-with-binary-file-uploading
+% 
+% Seongsik Park
+% seongsikpark@postech.ac.kr
 
-options = 'document';
+options = 'photo';
 if nargin > 0
     options = varargin{1};
 end
